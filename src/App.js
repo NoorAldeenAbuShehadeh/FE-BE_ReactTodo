f16a9165b3246cf20980cd524795aca823a7beeb
Noor Aldeen <anooraldeen9@gmail.com>
2023-05-15 14:43:28 +0300

    finish integration smple todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Container,
+  Typography,
+} from '@mui/material';
+import DoneIcon from '@mui/icons-material/Done';
+import axios from 'axios';
+import { Add, Delete } from '@mui/icons-material';
+import { fontSize } from '@mui/system';
+const TodoApp = () => {
+  const [todos, setTodos] = useState();
+  const [inputValue, setInputValue] = useState('');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+const fetchData=async()=>
+{
+   try {
+      const response = await axios.get('http://localhost:5500/todos/');
+      let data = await response.data
+      setTodos(data)
+    } catch (error) {
+      console.error(error);
+    }
 }
 
-export default App;
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = async() => {
+    if (inputValue.trim()) {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: Date.now(), title: inputValue },
+      ]);
+      setInputValue('');
+      try {
+      await axios.post(`http://localhost:5500/todos/`,{id: Date.now(), title: inputValue});
+    } catch (error) {
+      console.error(error);
+    }
+    }
+  };
+
+  const handleDeleteTodo = async(id) => {
+    try {
+      await axios.delete(`http://localhost:5500/todos/${id}`);
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCompleted=async(id)=>{
+        try {
+      await axios.put(`http://localhost:5500/todos/${id}`,{completion_status:'done'});
+      setTodos((prevTodos) => prevTodos.map((todo) => 
+        todo.id === id?{...todo , completion_status:'done'}:todo));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+    <Container sx={{marginTop:'20px',display:'flex',justifyContent:'center', fontSize:'26px',
+  fontWeight:'bold'}}>Simple Todo App</Container>  
+    <Container sx={{marginTop:'20px'}}>
+      <TextField
+        label="Add Todo"
+        variant="outlined"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<Add />}
+        onClick={handleAddTodo}
+        style={{    padding: '15px', marginLeft: '12px'}}
+      >
+        Add
+      </Button>
+      <List>
+        {todos?.map((todo) => (
+          <ListItem key={todo.id}>
+            <ListItemText primary={todo.title} />
+            {todo.completion_status==='done' ? (
+        <IconButton disabled>
+          <DoneIcon color='success' sx={{width:'40px',height:'30px'}}/>
+        </IconButton>
+      ) : (
+        <IconButton onClick={()=>handleCompleted(todo.id)}>
+          <DoneIcon sx={{width:'40px',height:'30px'}}/>
+        </IconButton>
+      )}
+            <ListItemSecondaryAction>
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+    </>
+  );
+};
+
+export default TodoApp;
